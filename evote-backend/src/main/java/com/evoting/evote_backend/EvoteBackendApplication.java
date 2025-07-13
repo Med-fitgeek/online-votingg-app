aed14f40169e15e213588e0bb17a339909272706
Medoune <ndongomedoune.ndiaye@gmail.com>
2025-07-13 20:16:05 +0200

    intialization du backend

---
+++ evote-backend/src/main/java/com/evoting/evote_backend/EvoteBackendApplication.java
@@ -0,0 +1,13 @@
+package com.evoting.evote_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EvoteBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EvoteBackendApplication.class, args);
+	}
+
+}
---
+++ evote-backend/src/test/java/com/evoting/evote_backend/EvoteBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.evoting.evote_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EvoteBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

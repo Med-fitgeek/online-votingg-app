0c6ec4b2c2c864e7930ce880a16e685db5ee9d0f
Medoune <ndongomedoune.ndiaye@gmail.com>
2025-07-30 10:18:39 +0200

    Mise a jour fichier pom.xml et correction des bugs lier aux mappers

--- evote-backend/src/main/java/com/evoting/evote_backend/mapper/ElectionMapper.java
+++ evote-backend/src/main/java/com/evoting/evote_backend/mapper/ElectionMapper.java
@@ -1,7 +1,9 @@
 package com.evoting.evote_backend.mapper;
 
 import com.evoting.evote_backend.dto.ElectionResponseDTO;
+import com.evoting.evote_backend.dto.OptionDTO;
 import com.evoting.evote_backend.entity.Election;
+import com.evoting.evote_backend.entity.Option;
 import org.mapstruct.Mapper;
 import org.mapstruct.factory.Mappers;
 
@@ -9,8 +11,7 @@ import java.util.List;
 
 @Mapper(componentModel = "spring")
 public interface ElectionMapper {
-    ElectionMapper INSTANCE = Mappers.getMapper(ElectionMapper.class);
-
     ElectionResponseDTO toDto(Election election);
     List<ElectionResponseDTO> toDtoList(List<Election> elections);
+    OptionDTO toDto(Option option);
 }

0bed3012acdb904f7045edf564f828e09fd86598
Medoune <ndongomedoune.ndiaye@gmail.com>
2025-07-30 10:20:08 +0200

    Amlioration du routage cote front

--- evote-frontend/src/app/app.component.ts
+++ evote-frontend/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from './shared/components/navbar/navbar.component'; // adapte le chemin selon ta structure
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NavbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- evote-frontend/src/app/app.routes.ts
+++ evote-frontend/src/app/app.routes.ts
@@ -1,3 +1,39 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './features/home/home.component';
+import { authGuard } from './core/guards/auth.guards';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    loadComponent: () => import('./features/home/home.component').then(m => m.HomeComponent)
+  },
+  {
+    path: 'register',
+    loadComponent: () => import('./features/auth/register/register.component').then(m => m.RegisterComponent)
+  },
+  {
+    path: 'login',
+    loadComponent: () => import('./features/auth/login/login.component').then(m => m.LoginComponent)
+  },
+  {
+    path: 'dashboard',
+    canActivate: [authGuard],
+    loadComponent: () =>
+        import('./features/dashboard/dashboard.component').then(m => m.DashboardComponent)
+  },
+  {
+    path: 'create-election',
+    canActivate: [authGuard],
+    loadComponent: () =>
+        import('./features/election/create-election/create-election.component').then(m => m.CreateElectionComponent)
+  },
+  {
+    path: 'my-elections',
+    canActivate: [authGuard],
+    loadComponent: () =>
+        import('./features/election/my-elections/my-elections.component').then(m => m.MyElectionsComponent)
+}
+
+
+];
+
